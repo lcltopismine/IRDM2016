98dc3b9397e5e3df01ebc42bfbe3b604d85cb500
rupchap <rupchap@gmail.com>
2016-04-15 11:30:49 +0100

    tidy up nn.py

--- code/nn.py
+++ code/nn.py
@@ -1,12 +1,9 @@
 import pandas as pd
-from datetime import datetime
 import numpy as np
-import scipy.sparse as sparse
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import FunctionTransformer
-from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.pipeline import FeatureUnion, make_pipeline
 
 from keras.layers.core import Dense, Dropout, Activation
 from keras.models import Sequential
@@ -25,27 +22,13 @@ def main():
     y_test = test_data[['value']].values
 
     # preprocess data - build in to a feature union
+    extract_and_onehot_zone = make_pipeline(FunctionTransformer(get_zone_col), OneHotEncoder())
+    extract_and_onehot_hour = make_pipeline(FunctionTransformer(get_hour_col), OneHotEncoder())
+    extract_and_onehot_day = make_pipeline(FunctionTransformer(get_day_col), OneHotEncoder())
+    extract_and_onehot_month = make_pipeline(FunctionTransformer(get_month_col), OneHotEncoder())
+    extract_and_onehot_holiday = make_pipeline(FunctionTransformer(get_holiday_col), OneHotEncoder())
+    extract_and_scale_temps = make_pipeline(FunctionTransformer(get_temp_cols), StandardScaler())
 
-    # Pipeline to convert Zone to one-hot
-    extract_and_onehot_zone = Pipeline([('extract_zone', FunctionTransformer(get_zone_col)),
-                                        ('onehot_zone', OneHotEncoder())])
-    # Pipeline to convert Hour to one-hot
-    extract_and_onehot_hour = Pipeline([('extract_hour', FunctionTransformer(get_hour_col)),
-                                        ('onehot_hour', OneHotEncoder())])
-    # Pipeline to convert Day to one-hot
-    extract_and_onehot_day = Pipeline([('extract_day', FunctionTransformer(get_day_col)),
-                                       ('onehot_day', OneHotEncoder())])
-    # Pipeline to convert Month to one-hot
-    extract_and_onehot_month = Pipeline([('extract_month', FunctionTransformer(get_month_col)),
-                                         ('onehot_month', OneHotEncoder())])
-    # Pipeline to convert Holiday to one-hot
-    extract_and_onehot_holiday = Pipeline([('extract_holiday', FunctionTransformer(get_holiday_col)),
-                                           ('onehot_holiday', OneHotEncoder())])
-    # Pipeline to scale temperatures
-    extract_and_scale_temps = Pipeline([('extract_temps', FunctionTransformer(get_temp_cols)),
-                                        ('scale_temps', StandardScaler())])
-
-    # FeatureUnion
     features = FeatureUnion(transformer_list=[('zone', extract_and_onehot_zone),
                                               ('hour', extract_and_onehot_hour),
                                               ('day', extract_and_onehot_day),
@@ -53,26 +36,22 @@ def main():
                                               ('holiday', extract_and_onehot_holiday),
                                               ('temps', extract_and_scale_temps)])
 
-    # PIPELINE - single element to call feature union.
-    pipe = Pipeline([('features', features)])
-
-    # process X data [toarray as keras requires dense inputs]
-    X = pipe.fit_transform(X_raw).toarray()  #keras requires dense
-    X_test = pipe.transform(X_test_raw).toarray()
+    # process X data [use toarray() as keras requires dense inputs]
+    print 'preprocess data'
+    X = features.fit_transform(X_raw).toarray()
+    X_test = features.transform(X_test_raw).toarray()
 
     input_dim = X.shape[1]
 
     # build neural network model
     model = Sequential()
 
-    model.add(Dense(input_dim*2, input_dim=input_dim))
+    model.add(Dense(input_dim*4, input_dim=input_dim))
     model.add(Activation('tanh'))
     model.add(Dropout(0.5))
-
-    model.add(Dense(input_dim*2, input_dim=input_dim))
+    model.add(Dense(input_dim*2))
     model.add(Activation('tanh'))
     model.add(Dropout(0.5))
-
     model.add(Dense(38))
     model.add(Dense(1))
 
@@ -92,10 +71,11 @@ def main():
     y_test_pred = model.predict(X_test, batch_size=100, verbose=1)
     zoneresults = test_data[['datetime', 'zone_id', 'weight', 'value']].copy()
     zoneresults['prediction'] = y_test_pred
+
     print 'Weighted Root Mean Square Error (including system level), test: %.5f' % WRMSE(zoneresults)
 
     print 'save results'
-    save_data_csv(zoneresults, outputfoldername+'nn_results.csv')
+    save_data_csv(zoneresults, outputfoldername+'nn_resultsTEMP.csv')
 
 
 def selectdata(df):
@@ -109,20 +89,13 @@ def selectdata(df):
 
 
 # preprocessing transformers
-
 # the below expect dense input - operate on input data
-def get_zone_col(X):
-    return X[:, 0].reshape((-1, 1))
-def get_hour_col(X):
-    return X[:, 1].reshape((-1, 1))
-def get_day_col(X):
-    return X[:, 2].reshape((-1, 1))
-def get_month_col(X):
-    return X[:, 3].reshape((-1, 1))
-def get_holiday_col(X):
-    return X[:, 4].reshape((-1, 1))
-def get_temp_cols(X):
-    return X[:, 5:16]
+def get_zone_col(X):    return X[:, 0].reshape((-1, 1))
+def get_hour_col(X):    return X[:, 1].reshape((-1, 1))
+def get_day_col(X):     return X[:, 2].reshape((-1, 1))
+def get_month_col(X):   return X[:, 3].reshape((-1, 1))
+def get_holiday_col(X): return X[:, 4].reshape((-1, 1))
+def get_temp_cols(X):   return X[:, 5:16]
 
 
 
