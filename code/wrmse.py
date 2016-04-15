67cd2291480a9ec9fdbcc52a2437b1ec240df394
rupchap <rupchap@gmail.com>
2016-04-15 12:50:42 +0100

    include code to save results to file

--- code/wrmse.py
+++ code/wrmse.py
@@ -1,6 +1,8 @@
 import pandas as pd
+from datetime import datetime
+from processandmergedata import save_data_csv
 
-def WRMSE(zoneresults):
+def WRMSE(zoneresults, saveresults=False, modelname=''):
     """
     Calculates Weighted Root Mean Square Error
     :param zoneresults: pandas dataframe including the columns 'datetime', 'zone_id', 'weight', 'value', 'prediction'
@@ -9,8 +11,8 @@ def WRMSE(zoneresults):
 
     # calculate errors - system results are just sum over all zones.
     zoneresults['error'] = zoneresults.value - zoneresults.prediction
-    sysresults = zoneresults.groupby('datetime')[['weight', 'error']].sum()
-
+    sysresults = zoneresults.groupby('datetime')[['weight', 'value', 'prediction', 'error']].sum()
+    sysresults.reset_index(inplace=True)
     # calculate square errors
     zoneresults['square_error'] = zoneresults.error ** 2
     sysresults['square_error'] = sysresults.error ** 2
@@ -23,5 +25,11 @@ def WRMSE(zoneresults):
     total_weighted_square_error = zoneresults.weighted_square_error.sum() + sysresults.weighted_square_error.sum()
     total_weights = zoneresults.weight.sum() + sysresults.weight.sum()
     wrmse = (total_weighted_square_error / total_weights) ** 0.5
+
+    if saveresults:
+        timestamp = datetime.now().strftime("%Y%M%d-%H%M%S")
+        save_data_csv(zoneresults, modelname+timestamp+'_zoneresults.csv')
+        save_data_csv(sysresults, modelname+timestamp+'_sysresults.csv')
+
 
     return wrmse
